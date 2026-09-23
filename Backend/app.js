c4789bac902690cfd1969a584aa64c4063ff5b7a
Siya Saxena <saxenasiya9@gmail.com>
2026-09-23 16:39:08 +0530

    first basic commit on setup of backend folder

---
+++ Backend/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+
+import cors from "cors";
+import connectToSocket from "./src/controllers/socketManager.js";
+import userRoutes from "./src/routes/user.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+//middleware configuration
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.set("port", process.env.PORT || 8000);
+
+app.get("/home", (req, res) => {
+  return res.json({ hello: "world" });
+});
+
+const start = async () => {
+  try {
+    const connectionDb = await mongoose.connect(
+      "mongodb+srv://<db_username>:Ui7M1tZu0WmyZMax@syncspace-cluster.nipsarr.mongodb.net/?appName=SyncSpace-Cluster ",
+    );
+    console.log("MONGO Connected DB Host: ${connectionDb.connection.host}");
+
+    server.listen(app.get("port"), () => {
+      console.log(`Server is running on port ${app.get("port")}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+start();
---
+++ Backend/src/models/user.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  token: {
+    type: String,
+    unique: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
